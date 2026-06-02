932be36058426cc56103ed40f26be03089358061
Kjaramillo05 <kjaramill12@utmachala.edu.ec>
2026-06-02 18:04:35 -0500

    Add initial Program.cs for BibliotecaSDD project

---
+++ specs/src/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BibliotecaSDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Proyecto Biblioteca con Spec Driven Development");
+        }
+    }
+}
