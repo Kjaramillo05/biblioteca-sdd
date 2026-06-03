4c4e2be7cd9240437afcefa31974e169f5e399d5
Kjaramillo05 <kjaramill12@utmachala.edu.ec>
2026-06-02 22:20:08 -0500

    Add BibliotecaSD.WinForms project with basic loan form

---
+++ BibliotecaSD.WinForms/BibliotecaSD.WinForms/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace BibliotecaSD.WinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private System.Windows.Forms.Label lblEstudiante;
+        private System.Windows.Forms.TextBox txtEstudiante;
+        private System.Windows.Forms.Label lblLibro;
+        private System.Windows.Forms.TextBox txtLibro;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.ListBox lstPrestamos;
+
+        private void InitializeComponent()
+        {
+            this.lblEstudiante = new System.Windows.Forms.Label();
+            this.txtEstudiante = new System.Windows.Forms.TextBox();
+            this.lblLibro = new System.Windows.Forms.Label();
+            this.txtLibro = new System.Windows.Forms.TextBox();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.lstPrestamos = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblEstudiante
+            // 
+            this.lblEstudiante.AutoSize = true;
+            this.lblEstudiante.Location = new System.Drawing.Point(12, 15);
+            this.lblEstudiante.Name = "lblEstudiante";
+            this.lblEstudiante.Size = new System.Drawing.Size(60, 13);
+            this.lblEstudiante.TabIndex = 0;
+            this.lblEstudiante.Text = "Estudiante:";
+            // 
+            // txtEstudiante
+            // 
+            this.txtEstudiante.Location = new System.Drawing.Point(90, 12);
+            this.txtEstudiante.Name = "txtEstudiante";
+            this.txtEstudiante.Size = new System.Drawing.Size(250, 20);
+            this.txtEstudiante.TabIndex = 1;
+            // 
+            // lblLibro
+            // 
+            this.lblLibro.AutoSize = true;
+            this.lblLibro.Location = new System.Drawing.Point(12, 45);
+            this.lblLibro.Name = "lblLibro";
+            this.lblLibro.Size = new System.Drawing.Size(33, 13);
+            this.lblLibro.TabIndex = 2;
+            this.lblLibro.Text = "Libro:";
+            // 
+            // txtLibro
+            // 
+            this.txtLibro.Location = new System.Drawing.Point(90, 42);
+            this.txtLibro.Name = "txtLibro";
+            this.txtLibro.Size = new System.Drawing.Size(250, 20);
+            this.txtLibro.TabIndex = 3;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(360, 12);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(120, 50);
+            this.btnRegistrar.TabIndex = 4;
+            this.btnRegistrar.Text = "Registrar préstamo";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // lstPrestamos
+            // 
+            this.lstPrestamos.FormattingEnabled = true;
+            this.lstPrestamos.Location = new System.Drawing.Point(15, 80);
+            this.lstPrestamos.Name = "lstPrestamos";
+            this.lstPrestamos.Size = new System.Drawing.Size(615, 303);
+            this.lstPrestamos.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(501, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 50);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Eliminar préstamo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 409);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblEstudiante);
+            this.Controls.Add(this.txtEstudiante);
+            this.Controls.Add(this.lblLibro);
+            this.Controls.Add(this.txtLibro);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.lstPrestamos);
+            this.Name = "Form1";
+            this.Text = "Biblioteca SD - Préstamos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ BibliotecaSD.WinForms/BibliotecaSD.WinForms/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BibliotecaSD.WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Inicialización adicional si es necesaria
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            string estudiante = txtEstudiante.Text;
+            string libro = txtLibro.Text;
+
+            if (string.IsNullOrWhiteSpace(estudiante) || string.IsNullOrWhiteSpace(libro))
+            {
+                MessageBox.Show("Debe ingresar estudiante y libro.");
+                return;
+            }
+
+            string registro = $"{estudiante} → {libro}";
+            lstPrestamos.Items.Add(registro);
+
+            txtEstudiante.Clear();
+            txtLibro.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Eliminar prestamo y actualizar la lista
+            if (lstPrestamos.SelectedItem != null)
+            {
+                lstPrestamos.Items.Remove(lstPrestamos.SelectedItem);
+            }
+            else
+            {
+                MessageBox.Show("Seleccione un préstamo para eliminar.");
+            }
+        }
+    }
+}
